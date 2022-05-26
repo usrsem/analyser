ec764312bb009ae788d9759cadd2a78c2c7817b0
usrsem <andrey7menov@gmail.com>
2022-05-26 23:49:48 +0300

    Adding unit test for orm

---
+++ tests/unit/test_orm.py
@@ -0,0 +1,41 @@
+import pytest
+from analyser.domain.dtos import Citizen, ImportDto, Gender
+from datetime import date
+from sqlalchemy.orm import Session
+from sqlalchemy.future import select
+from db.context import async_session, new_session
+
+
+@pytest.fixture
+def citizen():
+    return Citizen(
+        1,
+        "town1",
+        "street1",
+        "building1",
+        1,
+        "name1",
+        date.today(),
+        Gender.MALE,
+        1)
+
+
+async def test_citizen_mapper_can_save_row(citizen):
+    import_dto: ImportDto = ImportDto(1)
+
+    async_session.add(import_dto)
+    await async_session.commit()
+    
+    async_session.add(citizen)
+
+    after = await async_session.get(Citizen, (1, 1))
+
+    await async_session.delete(citizen)
+    await async_session.delete(import_dto)
+    await async_session.commit()
+
+    msg: str = f"{after = }, {citizen = }"
+
+    assert after == citizen, msg
+
+
