9286b87aa0af2935cfc4fa05fc2396368d733d93
usrsem <andrey7menov@gmail.com>
2022-05-26 23:49:14 +0300

    Adding ImportDto to sqlalchemy mappers

--- src/db/mappers.py
+++ src/db/mappers.py
@@ -1,7 +1,8 @@
-from analyser.domain.dtos import Citizen
+from analyser.domain.dtos import Citizen, ImportDto
 from sqlalchemy.orm import mapper
-from db.shemas import citizen_table
+from db.shemas import citizen_table, import_table
 
 def start_mappers():
     mapper(Citizen, citizen_table)
+    mapper(ImportDto, import_table)
 
