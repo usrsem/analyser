6534ebda076c770581b48f6d97fc7ef8fb1b9b76
usrsem <andrey7menov@gmail.com>
2022-05-27 12:46:23 +0300

    Refactoring test_orm with new conftest

--- tests/unit/test_orm.py
+++ tests/unit/test_orm.py
@@ -1,24 +1,8 @@
-import pytest
-from analyser.domain.dtos import Citizen, ImportDto, Gender
-from datetime import date
-from sqlalchemy.orm import Session
-from sqlalchemy.future import select
-from db.context import async_session, new_session
+from analyser.domain.dtos import Citizen, ImportDto
+from db.context import async_session
 
 
-@pytest.fixture
-def citizen():
-    return Citizen(
-        "town1",
-        "street1",
-        "building1",
-        1,
-        "name1",
-        date.today(),
-        Gender.MALE)
-
-
-async def test_citizen_mapper_can_save_row(citizen):
+async def test_citizen_mapper_can_save_row(citizen: Citizen):
     import_dto: ImportDto = ImportDto(citizen.import_id)
 
     async_session.add(import_dto)
@@ -37,5 +21,4 @@ async def test_citizen_mapper_can_save_row(citizen):
     msg: str = f"{after = }, {citizen = }"
 
     assert after == citizen, msg
-
 
