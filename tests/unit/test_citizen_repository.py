974f00eecef65016689ff4dd3700da58bdff0b92
usrsem <andrey7menov@gmail.com>
2022-05-27 16:03:47 +0300

    Adding tests for citizen repository

---
+++ tests/unit/test_citizen_repository.py
@@ -0,0 +1,83 @@
+import pytest
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from sqlalchemy.orm.session import Session
+
+from analyser.repository.citizen_repository import AsyncCitizenRepository, AsyncSessionCitizenRepository
+from analyser.domain.dtos import Citizen, ImportDto
+from db.context import session as asession
+from loguru import logger
+from typing import Optional
+
+
+@pytest.fixture
+async def repository(session):
+    yield AsyncSessionCitizenRepository(session, logger) # type: ignore
+
+
+async def test_save_citizens(
+    repository: AsyncCitizenRepository,
+    session: AsyncSession,
+    citizen: Citizen,
+) -> None:
+    await repository.save_citizens([citizen]) 
+    await session.commit()
+
+    mustbe = await session.get(
+        Citizen, (citizen.import_id, citizen.citizen_id))
+
+
+    await session.delete(citizen)
+    await session.commit()
+
+    assert citizen == mustbe
+
+
+async def test_find_by_citizen_id(
+    repository: AsyncCitizenRepository,
+    session: AsyncSession,
+    citizen: Citizen
+) -> None:
+    await repository.save_citizens([citizen])
+    await session.commit()
+
+
+    new_citizen: Optional[Citizen] = await repository.find_by_citizen_id(
+        citizen.citizen_id)
+
+    await session.delete(citizen)
+    await session.commit()
+
+    assert citizen == new_citizen
+
+
+async def test_delete_by_citizen_id(
+    repository: AsyncCitizenRepository,
+    session: AsyncSession,
+    citizen: Citizen
+) -> None:
+    await repository.save_citizens([citizen])
+    await session.commit()
+
+    await repository.delete_by_citizen_id(citizen.citizen_id)
+    await session.commit()
+
+    after: Optional[Citizen] = await repository.find_by_citizen_id(
+        citizen.citizen_id) 
+
+    await session.delete(citizen)
+    await session.commit()
+
+    assert after == None
+
+
+async def test_delete_by_citizen_empty_row(
+    repository: AsyncCitizenRepository,
+    session: AsyncSession,
+    citizen: Citizen
+) -> None:
+    try:
+        await repository.delete_by_citizen_id(citizen.citizen_id)
+    except SQLAlchemyError:
+        raise AssertionError()
+
