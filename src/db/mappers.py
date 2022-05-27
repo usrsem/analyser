71cb01194a40896a12ec8eb234da0707d98ed7b5
usrsem <andrey7menov@gmail.com>
2022-05-27 16:20:13 +0300

    Removing warnings about deleting empty rows

--- src/db/mappers.py
+++ src/db/mappers.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import mapper
 from db.shemas import citizen_table, import_table
 
 def start_mappers():
-    mapper(Citizen, citizen_table)
-    mapper(ImportDto, import_table)
+    mapper(Citizen, citizen_table, confirm_deleted_rows=False)
+    mapper(ImportDto, import_table, confirm_deleted_rows=False)
 
