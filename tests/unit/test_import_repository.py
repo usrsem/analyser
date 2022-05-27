cd5556ac27c8c163e85d53d060e8d780e313f13e
usrsem <andrey7menov@gmail.com>
2022-05-27 16:19:58 +0300

    Adding tests for import repository

---
+++ tests/unit/test_import_repository.py
@@ -0,0 +1,28 @@
+import pytest
+from analyser.domain.dtos import ImportDto
+from analyser.repository.import_repository import AsyncImportRepository, AsyncSessionImportRepository
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from loguru import logger
+
+
+@pytest.fixture
+def repository(session):
+    return AsyncSessionImportRepository(session, logger) # type: ignore
+
+
+async def test_saves_import(
+    repository: AsyncImportRepository,
+    session: AsyncSession
+) -> None:
+    import_dto = ImportDto()
+
+    await repository.save(import_dto)
+    await session.commit()
+
+    new_import = await session.get(ImportDto, import_dto.import_id)
+
+    await session.delete(import_dto)
+    await session.commit()
+
+    assert new_import == import_dto
+
