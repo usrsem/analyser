162542b05b03a5e29495c9b90bd2974f57b9e54b
usrsem <andrey7menov@gmail.com>
2022-05-27 12:40:46 +0300

    Updating migrations

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from alembic import context
+from alembic import context # type: ignore
 from db.shemas import metadata
 from analyser.config import DEFAULT_PG_URL
 
