d1119f139df6b5cccf15eba138b10d7a5a7d8155
usrsem <andrey7menov@gmail.com>
2022-05-28 03:22:24 +0300

    Adding fake citizen repository

--- src/analyser/repository/citizen_repository.py
+++ src/analyser/repository/citizen_repository.py
@@ -2,8 +2,10 @@ from uuid import UUID
 from analyser.domain.logger import Logger
 from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from analyser.domain.dtos import CitizenDto
+from analyser.domain.dtos import CitizenDto, ImportIdDto
 from typing import Iterable, Optional, Protocol
+
+from analyser.repository.import_repository import FakeImportRepository
 
 
 class AsyncCitizenRepository(Protocol):
@@ -18,7 +20,7 @@ class AsyncCitizenRepository(Protocol):
 
 
 class AsyncSessionCitizenRepository:
-    def __init__(self,session: AsyncSession) -> None:
+    def __init__(self, session: AsyncSession) -> None:
         self._session: AsyncSession = session
 
     async def save_all(self, citizens: Iterable[CitizenDto]) -> None:
@@ -36,4 +38,32 @@ class AsyncSessionCitizenRepository:
 
         if citizen is not None:
             await self._session.delete(citizen)
+
+
+class FakeCitizenRepository:
+    def __init__(
+        self,
+        cache: list[CitizenDto],
+        imports: FakeImportRepository
+    ) -> None:
+        self.cache: list[CitizenDto] = cache
+        self.imports: FakeImportRepository = imports
+
+    async def save_all(self, citizens: Iterable[CitizenDto]) -> None:
+        for citizen in citizens:
+            if ImportIdDto(citizen.import_id) not in self.imports.cache:
+                raise Exception("No such import_id in imports cache")
+
+        self.cache.extend(citizens)
+
+    async def find_by_citizen_id(self, citizen_id: UUID) -> Optional[CitizenDto]:
+        for citizen in self.cache:
+            if citizen.citizen_id == citizen_id:
+                return citizen
+
+    async def delete_by_citizen_id(self, citizen_id: UUID) -> None:
+        citizen = await self.find_by_citizen_id(citizen_id)
+
+        if citizen is not None:
+            self.cache.remove(citizen)
 
