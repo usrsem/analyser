ee369b1644b3d6c4b32136a5e42e73de5e5357ed
usrsem <andrey7menov@gmail.com>
2022-05-28 03:23:10 +0300

    Adding tests for imports service

---
+++ tests/unit/analyser/service/test_imports_service.py
@@ -0,0 +1,26 @@
+import pytest
+from analyser.domain.dtos import ImportDto
+from analyser.repository.repository_uow import FakeUnitOfWork, RepositoryUnitOfWork
+
+from analyser.service.imports_service import ImportService, V1ImportService
+
+
+@pytest.fixture
+def service():
+    return V1ImportService(FakeUnitOfWork())
+
+
+@pytest.fixture
+def import_dto(random_citizens_list):
+    dto = ImportDto(random_citizens_list)
+
+    for citizen in dto.citizens:
+        citizen.import_id = dto.import_id.import_id
+
+    return dto
+
+
+async def test_add_import(service, import_dto):
+    await service.add_import(import_dto)
+    assert service.uow.commited, f"{service.uow.commited}"
+
