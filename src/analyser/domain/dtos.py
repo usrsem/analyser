4aafa282739bab37b3f7ba65bf3060c75d859d9c
usrsem <andrey7menov@gmail.com>
2022-05-28 04:43:39 +0300

    Refactoring

--- src/analyser/domain/dtos.py
+++ src/analyser/domain/dtos.py
@@ -1,7 +1,7 @@
 import uuid
 from dataclasses import dataclass
 from datetime import date
-from enum import unique, Enum
+from enum import Enum
 
 from dataclasses import dataclass
 
@@ -34,5 +34,5 @@ class ImportIdDto:
 @dataclass
 class ImportDto:
     citizens: list[CitizenDto]
-    import_id: ImportIdDto = ImportIdDto()
+    import_id: ImportIdDto
 
