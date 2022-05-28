09c628950f4759034470a00ab0b0e04f7d62a077
usrsem <andrey7menov@gmail.com>
2022-05-28 03:01:11 +0300

    Adding generation uuid4 for model to dto mapping

--- src/analyser/domain/mappers.py
+++ src/analyser/domain/mappers.py
@@ -1,9 +1,12 @@
+from uuid import uuid4
 from analyser.domain.dtos import CitizenDto
 from analyser.domain.pydantic import CitizenModel
 from dataclasses import asdict
 
 
 def citizen_model_to_dto(m: CitizenModel) -> CitizenDto:
+    m_as_dict = dict(m)
+    m_as_dict["import_id"] = uuid4()
     return CitizenDto(**dict(m))
 
 
