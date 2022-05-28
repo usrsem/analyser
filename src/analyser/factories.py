1af6dea14016a8396c538af8628ea564e376e1e8
usrsem <andrey7menov@gmail.com>
2022-05-28 03:24:45 +0300

    Adding imports service factory

--- src/analyser/factories.py
+++ src/analyser/factories.py
@@ -1,13 +1,19 @@
 from loguru import logger
 from analyser.repository.citizen_repository import AsyncCitizenRepository, AsyncSessionCitizenRepository
 from analyser.repository.import_repository import AsyncImportRepository, AsyncSessionImportRepository
+from analyser.repository.repository_uow import SqlAlchemyUnitOfWork
+from analyser.service.imports_service import ImportsService, V1ImportsService
 from db.context import session
 
 
 def get_citizen_repository() -> AsyncCitizenRepository:
-    return AsyncSessionCitizenRepository(session(), logger)
+    return AsyncSessionCitizenRepository(session())
 
 
 def get_imports_repository() -> AsyncImportRepository:
-    return AsyncSessionImportRepository(session(), logger)
+    return AsyncSessionImportRepository(session())
+
+
+def get_imports_service() -> ImportsService:
+    return V1ImportsService(SqlAlchemyUnitOfWork())
 
