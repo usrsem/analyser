968634e354c61c5149506e209ac8d36a5311d7bd
usrsem <andrey7menov@gmail.com>
2022-05-28 04:45:24 +0300

    Adding one import_id to all citizens from import

--- src/analyser/service/imports_service.py
+++ src/analyser/service/imports_service.py
@@ -2,6 +2,7 @@ from typing import Protocol
 
 from analyser.domain.dtos import ImportDto, ImportIdDto
 from analyser.repository.repository_uow import RepositoryUnitOfWork
+from loguru import logger as log
 
 
 class ImportsService(Protocol):
@@ -14,6 +15,9 @@ class V1ImportsService:
         self.uow: RepositoryUnitOfWork = repository_uow
 
     async def add_import(self, import_dto: ImportDto) -> ImportIdDto:
+        for citizen in import_dto.citizens:
+            citizen.import_id = import_dto.import_id.import_id
+
         async with self.uow:
             await self.uow.imports.save(import_dto.import_id)
             await self.uow.commit()
