6cfb4222c09274331760c5e449fc4b5937cb34d3
usrsem <andrey7menov@gmail.com>
2022-05-28 04:57:52 +0300

    Fixing validation bugs in ImportModel

--- src/analyser/domain/pydantic.py
+++ src/analyser/domain/pydantic.py
@@ -1,6 +1,6 @@
 import uuid
 
-from pydantic.class_validators import validator
+from pydantic.class_validators import root_validator, validator
 from pydantic.error_wrappers import ValidationError
 
 from analyser.domain.dtos import Gender
@@ -45,37 +45,40 @@ class CitizenModel(BaseModel):
         if len(v) != len(set(v)):
             raise ValidationError('relatives must be unique', CitizenModel)
 
+        return v
+
 
 class ImportModel(BaseModel):
     citizens: list[CitizenModel]
 
-    # @validator("citizens")
-    # def validate_unique_citizen_id(cls, v):
-    #     citizen_ids = set()
-    #     for citizen in v:
-    #         if citizen.citizen_id in citizen_ids:
-    #             raise ValidationError(
-    #                 f"Citizen id {citizen.citizen_id} is not unique",
-    #                 CitizenModel
-    #             )
+    @root_validator
+    def validate_unique_citizen_id(cls, v):
+        citizen_ids = set()
+        for citizen in v.get("citizens"):
+            if citizen.citizen_id in citizen_ids:
+                raise ValidationError(
+                    f"Citizen id {citizen.citizen_id} is not unique",
+                    CitizenModel
+                )
 
-    #         citizen_ids.add(citizen.citizen_id)
+            citizen_ids.add(citizen.citizen_id)
 
-    #     return v
+        return v
 
-    # @validator("citizens")
-    # def validate_relatives(cls, v):
-    #     relatives = {
-    #         citizen.citizen_id: set(citizen.relatives)
-    #         for citizen in v
-    #     }
+    @root_validator
+    def validate_relatives(cls, v):
+        relatives = {
+            citizen.citizen_id: set(citizen.relatives)
+            for citizen in v.get("citizens")
+        }
 
-    #     for citizen_id, relative_ids in relatives.items():
-    #         for relative_id in relative_ids:
-    #             if citizen_id not in relatives.get(relative_id, set()):
-    #                 raise ValidationError(
-    #                     f"citizen {relative_id} does not have "
-    #                     f"relation with {citizen_id}",
-    #                     ImportModel
-    #                 )
+        for citizen_id, relative_ids in relatives.items():
+            for relative_id in relative_ids:
+                if citizen_id not in relatives.get(relative_id, set()):
+                    raise ValidationError(
+                        f"citizen {relative_id} does not have "
+                        f"relation with {citizen_id}",
+                        ImportModel)
+
+        return v
 
