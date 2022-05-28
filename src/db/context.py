830486022ec7e3a9eac59feffe75a57c1a63320e
usrsem <andrey7menov@gmail.com>
2022-05-28 04:44:25 +0300

    Refactoring

--- src/db/context.py
+++ src/db/context.py
@@ -1,25 +1,9 @@
 from asyncio import current_task
-from contextlib import asynccontextmanager
-from typing import AsyncGenerator
+from loguru import logger
 from sqlalchemy.ext.asyncio.engine import create_async_engine
 from sqlalchemy.orm import sessionmaker, Mapper, Query
 from sqlalchemy.ext.asyncio import AsyncSession, async_scoped_session
 from analyser.config import ASYNC_DEFAULT_PG_URL
-from db.mappers import start_mappers
-
-
-@asynccontextmanager
-async def new_session(**kwargs) -> AsyncGenerator:
-    new_session = _async_session_factory(**kwargs)
-
-    try:
-        yield new_session
-        await new_session.commit()
-    except:
-        await new_session.rollback()
-        raise
-    finally:
-        await new_session.close()
 
 
 def _get_query_cls(mapper, session):
@@ -38,23 +22,16 @@ def _get_query_cls(mapper, session):
     return Query(mapper, session)
 
 
-_engine = create_async_engine(ASYNC_DEFAULT_PG_URL, echo=True)
-
-_async_session_factory: sessionmaker = sessionmaker(
-    _engine,
-    query_cls=_get_query_cls,
-    expire_on_commit=False,
-    class_=AsyncSession)
-
-session: async_scoped_session = async_scoped_session(
-    _async_session_factory, scopefunc=current_task)
-
-
 DEFAULT_SESSION_FACTORY = sessionmaker(
     create_async_engine(ASYNC_DEFAULT_PG_URL, echo=True),
     query_cls=_get_query_cls,
     expire_on_commit=False,
     class_=AsyncSession)
 
-start_mappers()
+logger.info("Default session factory inited")
+
+session: async_scoped_session = async_scoped_session(
+    DEFAULT_SESSION_FACTORY, scopefunc=current_task)
+
+
 
