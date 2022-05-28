792b3d85496c0ec0d821f4288fdd6a1133ad687e
usrsem <andrey7menov@gmail.com>
2022-05-28 04:44:28 +0300

    Refactoring

--- src/db/mappers.py
+++ src/db/mappers.py
@@ -1,3 +1,4 @@
+from loguru import logger
 from analyser.domain.dtos import CitizenDto, ImportIdDto
 from sqlalchemy.orm import mapper
 from db.shemas import citizen_table, import_table
@@ -5,4 +6,5 @@ from db.shemas import citizen_table, import_table
 def start_mappers():
     mapper(CitizenDto, citizen_table, confirm_deleted_rows=False)
     mapper(ImportIdDto, import_table, confirm_deleted_rows=False)
+    logger.info("Mappers started")
 
