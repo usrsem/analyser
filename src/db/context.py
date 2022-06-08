7d8850bbea0fb0031db7d25419e6f1b375c47558
idevtier <idevtier@gmail.com>
2022-06-08 13:09:35 +0300

    Using config in db context

--- src/db/context.py
+++ src/db/context.py
@@ -3,7 +3,7 @@ from loguru import logger
 from sqlalchemy.ext.asyncio.engine import create_async_engine
 from sqlalchemy.orm import sessionmaker, Mapper, Query
 from sqlalchemy.ext.asyncio import AsyncSession, async_scoped_session
-from analyser.config import ASYNC_DEFAULT_PG_URL
+from analyser.config import settings
 
 
 def _get_query_cls(mapper, session):
@@ -21,12 +21,15 @@ def _get_query_cls(mapper, session):
 
     return Query(mapper, session)
 
+_pg_url = (f"postgresql+asyncpg://{settings.pg_user}:{settings.pg_password}"
+           f"@{settings.pg_host}/{settings.pg_db_name}")
 
 DEFAULT_SESSION_FACTORY = sessionmaker(
-    create_async_engine(ASYNC_DEFAULT_PG_URL, echo=True),
+    create_async_engine(_pg_url, echo=True),
     query_cls=_get_query_cls,
     expire_on_commit=False,
     class_=AsyncSession)
+
 
 logger.info("Default session factory inited")
 
