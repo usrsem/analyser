847832b22c3e9ed67d1c2d64a3341c10c63c9299
idevtier <idevtier@gmail.com>
2022-06-08 12:07:12 +0300

    Adding get import method to imports repository

--- src/analyser/repository/import_repository.py
+++ src/analyser/repository/import_repository.py
@@ -1,10 +1,17 @@
 from sqlalchemy.ext.asyncio.session import AsyncSession
-from analyser.domain.dtos import ImportIdDto
+from sqlalchemy.future import select
+from analyser.domain.dtos import CitizenDto, ImportDto, ImportIdDto
 from typing import Protocol
+
+from analyser.domain.pydantic import ImportModel
+from analyser.loader import log
 
 
 class AsyncImportRepository(Protocol):
     async def save(self, import_dto: ImportIdDto) -> None:
+        ...
+
+    async def get(self, import_id_dto: ImportIdDto) -> ImportDto:
         ...
 
 
@@ -15,6 +22,14 @@ class AsyncSessionImportRepository:
     async def save(self, import_dto: ImportIdDto) -> None:
         self._session.add(import_dto)
 
+    async def get(self, import_id_dto: ImportIdDto) -> ImportDto:
+        q = (select(CitizenDto)
+            .where(CitizenDto.import_id == import_id_dto.import_id))
+
+        result = await self._session.execute(q)
+
+        return ImportDto(result.scalars().all(), import_id_dto)
+
 
 class FakeImportRepository:
 
@@ -23,4 +38,7 @@ class FakeImportRepository:
 
     async def save(self, import_dto: ImportIdDto) -> None:
         self.cache.append(import_dto)
+
+    async def get(self, import_id_dto: ImportIdDto) -> ImportDto:
+        return None
 
