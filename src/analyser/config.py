2b1c671846b81d66f9d66881512e16b112559f52
idevtier <idevtier@gmail.com>
2022-06-08 13:09:23 +0300

    Adding config class

--- src/analyser/config.py
+++ src/analyser/config.py
@@ -1,3 +1,22 @@
+import os
+import pydantic as pc
+from pydantic import BaseSettings, Field
+
+
+class Settings(BaseSettings):
+    # pg_host: str = Field(..., env="PG_HOST")
+    pg_host: str = os.getenv(BaseSettings.Config.env_prefix + "PG_HOST")
+    pg_user: str = os.getenv(BaseSettings.Config.env_prefix + "PG_USER")
+    pg_password: str = os.getenv(BaseSettings.Config.env_prefix + "PG_PASSWORD")
+    pg_db_name: str = os.getenv(BaseSettings.Config.env_prefix + "PG_DB_NAME")
+
+    class Config:
+        env_prefix = 'ANALYSER_'
+
+
+settings = Settings()
+
+
 DEFAULT_PG_URL = "postgresql://postgres:password@localhost/enrollment"
 ASYNC_DEFAULT_PG_URL = "postgresql+asyncpg://postgres:password@localhost/enrollment"
 
